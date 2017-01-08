5d9908ecb8bb1e4bbfc4a7cdf7e314f8095d330c
Andrew Ferrier <andrewferrier@example.com>
2017-01-08 19:47:34 +0000

    Clone vader.vim as part of running tests - closes #14

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
+cd $REPO_ROOT
+
+if [[ ! -d vader.vim/ ]]; then
+    git clone https://github.com/junegunn/vader.vim.git
+fi
+
+cd $REPO_ROOT/tests
+
 vim -Nu <(cat << EOF
 filetype off
-set rtp+=~/.vim/plugged/vader.vim
+set rtp+=../vader.vim
 set rtp+=..
 filetype plugin indent on
 syntax enable
