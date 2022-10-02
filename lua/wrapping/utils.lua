81c2e1cfb1dbf2ba2ce725456d850fc943c059a5
Andrew Ferrier <andrewferrier@example.com>
2022-10-02 13:19:16 +0200

    fix: Use API functions to count blank lines
    This avoids ugly artifacts like selections showing up in the buffer

--- lua/wrapping/utils.lua
+++ lua/wrapping/utils.lua
@@ -12,13 +12,18 @@ M.get_buf_size = function()
     return count
 end
 
+-- FIXME: There may be a more efficient way to do this
 M.count_blank_lines = function()
-    local svpos = vim.fn.winsaveview()
-    vim.b.blankline_count = 0
-    -- silent is needed to ensure that even if there are no matches, we don't show an error.
-    vim.cmd("silent g/^\\s*$/let b:blankline_count += 1")
-    vim.fn.winrestview(svpos)
-    return vim.b.blankline_count
+    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
+
+    local count = 0
+    for _, line in pairs(lines) do
+        if string.match(line, "^%s*$") then
+            count = count + 1
+        end
+    end
+
+    return count
 end
 
 M.get_log_path = function()
